e77e94a55ba9ac0cae6a600d8b90f2fee6f1465c
Esteban Meza <meza.19@osu.edu>
2021-02-24 22:32:18 -0500

    refreshing the page brings back the last seached city

--- javascript.js
+++ javascript.js
@@ -1,16 +1,21 @@
 // jQuery initial setup of application
-var days = 0, temperature, humidty, windSpeed, lat, lon, uv, icon, cityInput;
+var days = 0, temperature, humidty, windSpeed, lat, lon, uv, icon, cityInput, lastSelectedState;
 $( window ).on( "load", firstData(cityInput) );
 
 // firstData() loads once on load of the program to set intitial setting for the application
 // sets columbus as the first city and allows for other quick cities to be chosen as well as
-// seperate zip can be submitted
+// seperate city can be submitted
 function firstData(input) {
-    // sets intial zip to columbus
+    // sets intial city to columbus
+    let stateStore = JSON.parse(localStorage.getItem("lastSelectedState"));
     if (input==null ) {
     input = "columbus";
+        if (stateStore !== null){
+        input = stateStore;
+        }
     }
-
+   
+    
     //pulls info from api
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + input + "&units=imperial&appid=c8d528298be5665ca591ee78f5afcc10";
     //    "https://api.openweathermap.org/data/2.5/forecast/daily?q=" +  input  + "&cnt=5&appid=c8d528298be5665ca591ee78f5afcc10";
@@ -37,13 +42,21 @@ function firstData(input) {
         $(".temp").text("Temperature: "+ response.main.temp + " °F");
         $(".humidity").text("Humidity: " + response.main.humidity);
         $(".wind").text("Wind Speed: " + response.wind.speed + " MPH");
-    
+        
         lon = response.coord.lon;
         lat = response.coord.lat;
         getUV(lat,lon);
+
+        // $(".everyday").each(function(index) {
+        //     this.empty();
+        // });
+     
         setFiveDay();
 
     })
+    console.log("pre");
+    localStorage.setItem("lastSelectedState", JSON.stringify(input));
+    console.log("post");
 }
 
  // Populates info based after the search button is clicked
@@ -69,7 +82,7 @@ function getUV(lt, ln){
 var uvURL= "http://api.openweathermap.org/data/2.5/uvi?lat=" + lt + "&lon=" + ln + "&appid=c8d528298be5665ca591ee78f5afcc10";
 $.ajax({url:uvURL , method: "GET"}).then(function(response) {
     uv = response.value;
-     uv = 7;
+    
     
     
     $(".uvText").text("UV index: ");
@@ -87,13 +100,16 @@ $.ajax({url:uvURL , method: "GET"}).then(function(response) {
 function setFiveDay() {
     // pulls daily weather for city
     var fiveURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat+ "&lon=" + lon + "&exclude=current,minutely,hourly,alerts&units=imperial&appid=c8d528298be5665ca591ee78f5afcc10"; 
-
+    days = 0;
+    
     // test for five day pull
     $.ajax({url:fiveURL , method: "GET"}).then(function(response) {
         
         //populates five day weather forecast
         $(".everyday").each(function(index) {
+            
             days += 1;
+            console.log(days);
             let dailyTemp = response.daily[days].temp.day;
             iconName= response.daily[days].weather[0].icon;
             iconURL = "http://openweathermap.org/img/w/" + iconName + ".png";
@@ -104,7 +120,7 @@ function setFiveDay() {
             var nameOfDay = moment().add(days, 'day').format("dddd");
             
             nameOfDay = nameOfDay.substring(0,3);
-            
+            $(this).empty();
             $(this).append(nameOfDay);
             $(this).append(iconDisplay);
             $(this).append($("<p>").text(dailyTemp + "°F"));
